00f53a794688c1a83741ab13898862dc7e46d110
Rahil Jain <f20170541@pilani.bits-pilani.ac.in>
2025-01-08 16:27:44 +0530

    gcp connect

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 
-const backend = "https://agate-detailed-vase.glitch.me";
+// const backend = "https://agate-detailed-vase.glitch.me";
+const backend = "https://chalk3-702959074493.asia-south1.run.app";
 // const backend = "http://127.0.0.1:8000";
 export { backend };
